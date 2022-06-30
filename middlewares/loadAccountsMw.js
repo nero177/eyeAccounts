28005d878bed15e94d68ce81c3ee6c958187c9f7
nero177 <89150623+nero177@users.noreply.github.com>
2022-06-30 19:50:53 +0300

    sdfsd

--- middlewares/loadAccountsMw.js
+++ middlewares/loadAccountsMw.js
@@ -1,9 +1,11 @@
 const accountModel = require('../models/Account');
+const axios = require('axios').default;
 
 module.exports = async function(req, res, next) {
     try {
         const accounts = await accountModel.find();
         res.locals.accounts = accounts;
+
         next();
     } catch (err) {
         console.log(err)
--- routers/router.js
+++ routers/router.js
@@ -5,13 +5,15 @@ const bodyParser = require('body-parser');
 const urlencoded = bodyParser.urlencoded({ extended: false });
 const { regValidation, loginValidation } = require('../validationArrays');
 const loadAccountsMw = require('../middlewares/loadAccountsMw');
+const path = require('path');
 
 router.get('/', loadAccountsMw, (req, res) => res.render('index', { accounts: res.locals.accounts }))
 router.get('/reg', (req, res) => res.render('registration'))
 router.get('/login', (req, res) => res.render('login'))
-router.get('/logout', userController.logout);
+router.get('/logout', userController.logout)
 router.get('/oplata', (req, res) => res.render('oplata'))
 router.get('*', (req, res) => res.render('404'));
+
 
 
 router.post('/reg', regValidation, urlencoded, userController.registration)
